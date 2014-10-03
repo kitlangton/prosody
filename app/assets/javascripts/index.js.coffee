06b0eaa4bc343a65c295d303ba8cd67ad79a90fb
Kit Langton <kitlangton@gmail.com>
2014-10-03 11:02:55 -0400

    Fade in index page

--- app/assets/javascripts/index.js.coffee
+++ app/assets/javascripts/index.js.coffee
@@ -1,2 +1,5 @@
 $ ->
   $(".post .body").dotdotdot()
+  $(".post").each (index) ->
+    $(this).delay(40*index).transition
+      opacity: .8,
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+
+  before_save :post_titleize
+
+  private
+
+  def post_titleize
+    self.title = title.titleize
+  end
 end
