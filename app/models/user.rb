2250d4a6be893b591e899d17dfa2676dd52ad37d
Kit Langton <kitlangton@gmail.com>
2014-10-20 10:49:06 -0400

    Extract NDP

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
+  require 'nom_de_plume'
   has_many :comments
   has_many :haikus
   has_many :posts
   before_create :concoct_nom_de_plume
-  require 'ruby-dictionary'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
@@ -20,30 +20,8 @@ class User < ActiveRecord::Base
   end
 
   def concoct_nom_de_plume
-    ndp = generate_nom_de_plume
+    ndp = NomDePlume.new(self).generate
     self.nom_de_plume = ndp.titleize
   end
 
-  def generate_nom_de_plume
-    first = generate_word_from first_name.to_s
-    last = generate_word_from last_name.to_s
-    ndp = [first,last].join(" ")
-  end
-
-  private
-
-
-  def generate_word_from (name)
-    dictionary = Dictionary.from_file('./lib/SuperDict.txt')
-    length = name.length
-
-    length.times do |i|
-      values = dictionary.starting_with(name.to_s[0..length-i])
-      values.select! { |word| word.length < 8 }
-      if values.length > 10
-        random_word = Random.rand(values.length)
-        return values[random_word]
-      end
-    end
-  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,4 @@
 require 'rails_helper'
-
 RSpec.describe User, :type => :model do
-  describe "It generates a random nom de plume" do
-    it "should equal something" do
-      @user = User.create({
-        first_name:"Leanna",
-        last_name:"Grennan",
-        email:"hi@hihihi.com",
-        password:"123123123",
-        password_confirmation:"123123123",
-      })
-      expect(@user.nom_de_plume).to match /\w+ \w+/
-    end
-  end
+  describe "It generates a random nom de plume"
 end
